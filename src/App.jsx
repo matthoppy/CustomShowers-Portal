08aebcdc8a50171b3d6d260e1be1e2e586083646
Matthew Hopkinson <matthew_hopkinson@ymail.com>
2026-03-13 23:58:02 +0000

    Remove auth - open CRM without login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import ProtectedRoute from './components/layout/ProtectedRoute'
-import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import LeadsList from './pages/leads/LeadsList'
 import LeadDetail from './pages/leads/LeadDetail'
@@ -19,29 +16,24 @@ import InvoiceDetail from './pages/invoices/InvoiceDetail'
 export default function App() {
   return (
     <HashRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/leads" element={<LeadsList />} />
-            <Route path="/leads/:id" element={<LeadDetail />} />
-            <Route path="/customers" element={<CustomersList />} />
-            <Route path="/customers/:id" element={<CustomerDetail />} />
-            <Route path="/quotes" element={<QuotesList />} />
-            <Route path="/quotes/new" element={<QuoteForm />} />
-            <Route path="/quotes/:id/edit" element={<QuoteForm />} />
-            <Route path="/quotes/:id" element={<QuoteDetail />} />
-            <Route path="/jobs" element={<JobsList />} />
-            <Route path="/jobs/:id" element={<JobDetail />} />
-            <Route path="/invoices" element={<InvoicesList />} />
-            <Route path="/invoices/new" element={<InvoiceForm />} />
-            <Route path="/invoices/:id/edit" element={<InvoiceForm />} />
-            <Route path="/invoices/:id" element={<InvoiceDetail />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/leads" element={<LeadsList />} />
+        <Route path="/leads/:id" element={<LeadDetail />} />
+        <Route path="/customers" element={<CustomersList />} />
+        <Route path="/customers/:id" element={<CustomerDetail />} />
+        <Route path="/quotes" element={<QuotesList />} />
+        <Route path="/quotes/new" element={<QuoteForm />} />
+        <Route path="/quotes/:id/edit" element={<QuoteForm />} />
+        <Route path="/quotes/:id" element={<QuoteDetail />} />
+        <Route path="/jobs" element={<JobsList />} />
+        <Route path="/jobs/:id" element={<JobDetail />} />
+        <Route path="/invoices" element={<InvoicesList />} />
+        <Route path="/invoices/new" element={<InvoiceForm />} />
+        <Route path="/invoices/:id/edit" element={<InvoiceForm />} />
+        <Route path="/invoices/:id" element={<InvoiceDetail />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </HashRouter>
   )
 }
